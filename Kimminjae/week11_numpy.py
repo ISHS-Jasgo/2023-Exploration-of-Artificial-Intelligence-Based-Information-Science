002996f52f7024bd3af53d1437e7a1928ad6ba7e
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-05-31 20:44:32 +0900

    week11 numpy 06

--- Kimminjae/week11_numpy.py
+++ Kimminjae/week11_numpy.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-d2_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# list style
-print(d2_array[2][0], d2_array[-1][-3], d2_array[2][-3])
-# numpy style
-print(d2_array[2, 0], d2_array[-1, 0])
+d2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+print(d2)
 
-d2_array[0, 0] = 11
-print(d2_array)
+print(d2[::2][::2])
+print(d2[::2, ::2])
 
-d2_array[0, 0] = 22.74
-print(d2_array)
+print(d2[0:2, 2:])
+print(d2[0])
+print(d2[1], d2[1, :])
+print(d2[:, 2:3])
+print(d2[:, 2])
+print(d2[0:2, 0:2])
+print(d2[1::2, 1::2])
